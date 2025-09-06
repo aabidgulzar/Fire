d25d6d41cf5eaffe43f8e1da6db4d2a2ee983e58
Abid Gulzar <abidgulzar234@gmail.com>
2025-09-06 12:53:22 +0530

    first commit

---
+++ fire.js
@@ -0,0 +1,17 @@
+ let light = document.getElementById("light");
+        let scale = document.getElementById("scale");
+        let stick = document.getElementById("stick");
+        let cursor = document.getElementById("cursor");
+        addEventListener('mousemove', handleEvent);
+        function handleEvent(event) {
+            const x = event.clientX;
+            const y = event.clientY;
+            light.style.left = x-110 + "px";
+            light.style.top = y-125 + "px";
+
+            scale.style.left = x-110 + "px";
+            scale.style.top = y-125 + "px";
+            
+            stick.style.left = x-110 + "px";
+            stick.style.top = y-125 + "px";
+        }
